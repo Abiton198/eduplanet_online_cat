02797f581c656ad5aacfc43222d0a7344bf8c2cd
Abiton198 <abitonp@gmail.com>
2026-06-05 06:35:42 +0200

    api route updated

--- src/utils/AIExamMocker.jsx
+++ src/utils/AIExamMocker.jsx
@@ -8,7 +8,7 @@ import {
 } from "firebase/firestore";
 
 // const API = "https://chatbot-backend-educat.onrender.com";
-const API = import.meta.env.VITE_API_URL || "http://localhost:5000";
+const API = import.meta.env.VITE_API_URL;
 import { useStudentId } from "./StudentId";
 import { useParams } from "react-router-dom";
 
